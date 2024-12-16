9b3e0da0e1adcf0ff329371bf0b7ce5798e1001d
김별 <@DESKTOP-IH5VFIV>
2024-12-17 05:08:23 +0900

    24.12.17 백준

---
+++ Baekjoon/src/q10926/Main.java
@@ -0,0 +1,20 @@
+package q10926;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.next();
+		
+		sc.close();
+		
+		String surprise = "??!";
+		
+		System.out.print(id+surprise);
+
+	}
+
+}
---
+++ Baekjoon/src/q18109/Main.java
@@ -0,0 +1,20 @@
+package q18109;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int Thailand = sc.nextInt();
+		
+		sc.close();
+		
+		int korea = Thailand-543;
+		
+		System.out.print(korea);
+
+	}
+
+}
